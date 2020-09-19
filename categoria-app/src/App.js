d3d8da50bb8d01048cfa87df6672b3b4e2e20f2c
Diego <diego.canedo86@gmail.com>
2020-09-18 22:37:53 -0300

    Incluindo component footer

--- categoria-app/src/App.js
+++ categoria-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import NavApp from './components/nav/NavApp'
+import NavApp from './components/Nav/NavApp'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <NavApp/>
+      <Footer/>
     </div>
   );
 }
---
+++ categoria-app/src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Navbar from 'react-bootstrap/Navbar';
+import NavbarBrand from 'react-bootstrap/NavbarBrand';
+import Container from 'react-bootstrap/Container';
+
+
+const Footer = () => {
+    return (
+        <div className="fixed-bottom">  
+        <Navbar color="dark" dark>
+            <Container>
+                <NavbarBrand>Footer</NavbarBrand>
+            </Container>
+        </Navbar>
+        </div>
+    )
+}
+
+export default Footer;
+
+
