00a7c3c4575db3adb5b4832ee5320b85b60f04a2
Leonardo <leo.licht.von@gmail.com>
2020-09-21 21:37:01 -0300

    atualizando

--- categoria-app/src/components/Card/styles/Card.js
+++ categoria-app/src/components/Card/styles/Card.js
@@ -16,7 +16,13 @@ export const Image = styled.img`
     width: 100%;
 `;
 export const Button = styled.button`
-    cursor: pointer;
-    display: block;
-    text:align: center;
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
 `;
+
