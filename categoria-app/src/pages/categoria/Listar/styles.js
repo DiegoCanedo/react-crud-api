fad5b201c680ab2525424492388a1dc4ec6d0585
Diego <diego.canedo86@gmail.com>
2020-09-23 15:55:59 -0300

    alterando margin da listagem

--- categoria-app/src/pages/categoria/Listar/styles.js
+++ categoria-app/src/pages/categoria/Listar/styles.js
@@ -8,7 +8,7 @@ background-color: #ffffff;
 border-radius: 10px;
 box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
 max-width: 100%;
-margin: 20px;
+margin: 20px 0 20px;
 overflow: hidden;
 width: 400px;
 `;
